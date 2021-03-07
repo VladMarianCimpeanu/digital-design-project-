542ad222b282cdcbfcd0c5c2bf0cf8109dc969dc
romcruise-sudo <62434812+romcruise-sudo@users.noreply.github.com>
2021-03-07 20:36:14 +0100

    fixed the rendering loop

--- render.py
+++ render.py
@@ -1,6 +1,6 @@
 import pygame
 
-FILE_TO_RENDER = 'output.txt' # change this line to choose which file has to be rendered
+FILE_TO_RENDER = 'output_testbench.txt' # change this line to choose which file has to be rendered
 SIZE_WINDOW = 128 * 8
 SIZE_PIXEL = 8
 info_img = []
@@ -10,7 +10,7 @@ run = True
 pygame.init()
 
 window = pygame.display.set_mode((SIZE_WINDOW, SIZE_WINDOW))
-pygame.display.set_caption("input_rendered")
+pygame.display.set_caption("INPUT_RENDERED")
 with open(FILE_TO_RENDER) as reader:
     for line in reader:
         info_img.append(int(line))
@@ -23,10 +23,10 @@ while run:
     pygame.display.update()
     if isLoaded:
         i = 0
-        while i < info_img[1]:
+        while i < info_img[2]:
             j = 0
-            while j < info_img[0]:
-                start_row = i * info_img[0]
+            while j < info_img[1]:
+                start_row = i * info_img[1]
                 color = info_img[start_row + j + 2]
                 x = j * SIZE_PIXEL
                 y = i * SIZE_PIXEL

f348bd7a07420f488d91d7d9a71211987c55f15c
Vlad Marian Cimpeanu <62434812+VladMarianCimpeanu@users.noreply.github.com>
2021-04-28 21:27:35 +0200

    udpated format_generator.py

--- python/format_generator.py
+++ python/format_generator.py
@@ -1,13 +1,29 @@
 from PIL import Image
+import click
 
-NAME_IMAGE = 'low_contrast2.jpg' # name of the image has to be decoded
-OUTPUT_FILE = 'input.txt'
 
-picture = Image.open(NAME_IMAGE, 'r')
-columns, rows = picture.size
-pixel_values = list(picture.getdata())
-with open(OUTPUT_FILE, 'w') as writer:
-    dimension = columns * rows
-    writer.write(str(dimension) + '\n' + str(columns) + '\n' + str(rows))
-    for pixel in pixel_values:
-        writer.write('\n' + str(pixel[0]))
+@click.command()
+@click.option('--file', default='1', help='Path of the image that you want to convert.')
+def generate(file):
+    if file == 1:
+        NAME_IMAGE = 'imageToDecode.jpg'  # -------> name of the default image, change this line if you want to save
+        # a default path
+    else:
+        NAME_IMAGE = file
+    OUTPUT_FILE = 'input.txt'
+    try:
+        picture = Image.open(NAME_IMAGE, 'r')
+        columns, rows = picture.size
+        pixel_values = list(picture.getdata())
+        with open(OUTPUT_FILE, 'w') as writer:
+            dimension = columns * rows
+            writer.write(str(dimension) + '\n' + str(columns) + '\n' + str(rows))
+            for pixel in pixel_values:
+                writer.write('\n' + str(pixel[0]))
+        click.echo("Conversion ended successfully.")
+    except FileNotFoundError:
+        click.echo("No such file exists.")
+
+
+if __name__ == '__main__':
+    generate()
